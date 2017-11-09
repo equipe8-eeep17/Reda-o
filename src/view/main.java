cd4875f73e03bee4e29fb3ac455881d87e76b01b
r17lu <r17lu@DESKTOP-T27L4L8>
2017-11-09 16:15:36 -0200

    1 comit

---
+++ src/view/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author r17lu
+ */
+public class main {
+    public static void main(String args []){
+        Tela2 t2 = new Tela2();
+        t2.setVisible(true);
+        t2.setLocationRelativeTo(null);
+    }
+}
